4d32c60d30d69782023dc77d3627b5919aaf1b94
viresh rawool <vireshrawool007@gmail.com>
2021-01-26 16:39:59 +0530

    calulated wage for month

--- demo.sh
+++ demo.sh
@@ -1,22 +1,29 @@
 #!/bin/bash -x
-random=$((RANDOM%3))
 isFullTime=2
 isPartTime=1
 absent=0
 empRatePerHr=20;
-
-case $random in
-                $isFullTime )
-                        empHrs=8
-                        ;;
-                $isPartTime )
-                        empHrs=4
-                        ;;
-                *)
-                        empHrs=0
-                        ;;
-esac
-salary=$((empHrs*empRatePerHr))
+workingday=0
+while [ $workingday -le 20 ]
+do
+	random=$((RANDOM%3))
+	case $random in
+	                $isFullTime )
+	                        empHrs=8
+	                        ;;
+        	        $isPartTime )
+                	        empHrs=4
+                        	;;
+	                *)
+        	                empHrs=0
+                	        ;;
+	esac
+	salary=$((salary + empHrs*empRatePerHr))
+	if [ $random -ne 0 ]
+	then
+		((workingday++))
+	fi
+done
 echo $salary
 
 
