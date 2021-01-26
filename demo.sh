b9a952e660dfda2d80091c6b14311aa3da5658b5
viresh rawool <vireshrawool007@gmail.com>
2021-01-26 16:48:24 +0530

    added condition of max working hr or working day

--- demo.sh
+++ demo.sh
@@ -4,7 +4,10 @@ isPartTime=1
 absent=0
 empRatePerHr=20;
 workingday=0
-while [ $workingday -le 20 ]
+workinghr=0
+max_working_day=20
+max_working_hr=50
+while [ $workingday -le $max_working_day ] && [ $workinghr -le $max_working_hr ]
 do
 	random=$((RANDOM%3))
 	case $random in
@@ -18,6 +21,7 @@ do
         	                empHrs=0
                 	        ;;
 	esac
+	workinghr=$((workinghr + empHrs))
 	salary=$((salary + empHrs*empRatePerHr))
 	if [ $random -ne 0 ]
 	then
