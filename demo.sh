971c0c739b9b2e65ba1d9b94fa7d2e856ad7aad7
viresh rawool <vireshrawool007@gmail.com>
2021-01-26 16:10:59 +0530

    added daily wage for part time

--- demo.sh
+++ demo.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -x
-random=$((RANDOM%2))
-full=1
+random=$((RANDOM%3))
+full=2
+part=1
 absent=0
 if [ $random = $full ];
 then
         rateperhr=20
         emphr=8
         salary=$(( $rateperhr * $emphr))
+elif [ $random = $part ];
+then
+        ratePerHr=20
+        empHr=4
+        salary=$(( $ratePerHr * $empHr))
 else
         salary=0
 fi
-
 
