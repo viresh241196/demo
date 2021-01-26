aff75f74ff488d6b85da11f6694dd408f81f55d6
viresh rawool <vireshrawool007@gmail.com>
2021-01-26 16:59:49 +0530

    create a function to get work hours

--- demo.sh
+++ demo.sh
@@ -7,20 +7,25 @@ workingday=0
 workinghr=0
 max_working_day=20
 max_working_hr=50
+function cal_work_hr () {
+        case $1 in
+                $isFullTime )
+                        empHrs=8
+                        ;;
+                $isPartTime )
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        echo $empHrs
+}
+
 while [ $workingday -le $max_working_day ] && [ $workinghr -le $max_working_hr ]
 do
 	random=$((RANDOM%3))
-	case $random in
-	                $isFullTime )
-	                        empHrs=8
-	                        ;;
-        	        $isPartTime )
-                	        empHrs=4
-                        	;;
-	                *)
-        	                empHrs=0
-                	        ;;
-	esac
+	empHrs="$( cal_work_hr $random )"
 	workinghr=$((workinghr + empHrs))
 	salary=$((salary + empHrs*empRatePerHr))
 	if [ $random -ne 0 ]
