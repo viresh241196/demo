e01548b7261143b2559dd2f66788d3bfa4303d74
viresh rawool <vireshrawool007@gmail.com>
2021-01-27 15:47:14 +0530

    create a store dictionary and stored all thre result

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -2,7 +2,13 @@
 read -p 'enter first number ' a
 read -p 'enter second number ' b
 read -p 'enter third number ' c
-operation1=$((a + b * c)
+declare -A store
+
+operation1=$((a + b * c))
 operation2=$((a*b +c))
 operation3=$((c+a/b))
 operation4=$((a%b+c))
+store[1]=$operation1
+store[2]=$operation2
+store[3]=$operation3
+store[4]=$operation4
