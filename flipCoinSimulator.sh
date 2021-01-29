409f0f20abc6fb2d992bb912a1584fb0b78551d7
viresh rawool <vireshrawool007@gmail.com>
2021-01-29 12:48:15 +0530

    created a for loop to get multiple results and added counter to count the outcome

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -x
-result=$((RANDOM%2))
-if [ $result -eq 0 ]
-then
-	echo "heads"
-else
-	echo "tails"
-fi
+count_of_heads=0
+count_of_tails=0
+for (( i=0;i<=10;i++))
+do
+	result=$((RANDOM%2))
+	if [ $result -eq 0 ]
+	then
+		((count_of_heads++))
+	else
+		((count_of_tails++))
+	fi
+done
+echo $count_of_heads
+echo $count_of_tails
