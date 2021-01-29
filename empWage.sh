965f12c90ab14cd8af572d7aac239c8d7885d3b3
viresh rawool <vireshrawool007@gmail.com>
2021-01-29 19:16:39 +0530

    renamed the file to empWage

---
+++ empWage.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+isFullTime=2
+isPartTime=1
+absent=0
+empRatePerHr=20;
+workingday=0
+workinghr=0
+max_working_day=20
+max_working_hr=50
+da_counter=1
+function cal_work_hr () {
+        case $1 in
+                $isFullTime )
+                        empHrs=8
+                        ;;
+                $isPartTime )
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        echo $empHrs
+}
+
+while [ $workingday -le $max_working_day ] && [ $workinghr -le $max_working_hr ]
+do
+	random=$((RANDOM%3))
+	empHrs="$( cal_work_hr $random )"
+	workinghr=$((workinghr + empHrs))
+	salary=$((salary + empHrs*empRatePerHr))
+	daily_wage[ ((day_counter++))]=$salary
+	if [ $random -ne 0 ]
+	then
+		((workingday++))
+	fi
+done
+echo $salary
+echo ${daily_wage[@]}
+echo ${!daily_wage[@]}
+
